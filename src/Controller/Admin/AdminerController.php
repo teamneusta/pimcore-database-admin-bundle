d22697e0e318c16c268a06e130bf56f8f23ff371
Jacob Dreesen <j.dreesen@neusta.de>
2025-08-06 13:39:57 +0200

    Some cleanup & improvements

--- src/Controller/Admin/AdminerController.php
+++ src/Controller/Admin/AdminerController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Neusta\Pimcore\DatabaseAdminBundle\Controller\Admin {
     use Pimcore\Controller\KernelControllerEventInterface;
     use Pimcore\Controller\UserAwareController;
-    use Pimcore\Tool\Session;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\HttpKernel\Event\ControllerEvent;
@@ -21,24 +20,22 @@ namespace Neusta\Pimcore\DatabaseAdminBundle\Controller\Admin {
         #[Route('/admin/external_adminer/adminer', name: 'neusta_database_admin_adminer_index')]
         public function index(?Profiler $profiler): Response
         {
-            if ($profiler) {
-                $profiler->disable();
+            $profiler?->disable();
+
+            // disable the debug error handler while including adminer
+            set_error_handler(function () {});
+
+            try {
+                chdir($this->adminerHome . 'adminer');
+
+                ob_start();
+                require $this->adminerHome . 'adminer/index.php';
+                $content = ob_get_clean();
+            } finally {
+                restore_error_handler();
             }
 
-            // disable debug error handler while including adminer
-            $errorHandler = set_error_handler(function () {
-            });
-
-            chdir($this->adminerHome . 'adminer');
-            include($this->adminerHome . 'adminer/index.php');
-
-            set_error_handler($errorHandler);
-
-            // empty fake response, unfortunately Adminer uses flush() very heavily so we're not able to buffer, rewrite
-            // and put the into a proper response object :(
-            $response = new Response();
-
-            return $this->mergeAdminerHeaders($response);
+            return $this->mergeAdminerHeaders(new Response($content));
         }
 
         #[Route('/admin/external_adminer/{path}', name: 'neusta_database_admin_adminer_proxy', requirements: ['path' => '.*'])]
@@ -50,9 +47,9 @@ namespace Neusta\Pimcore\DatabaseAdminBundle\Controller\Admin {
             $content = '';
 
             // proxy for resources
-            $path = $request->get('path');
+            $path = $request->attributes->get('path');
             if (preg_match("@\.(css|js|ico|png|jpg|gif)$@", $path)) {
-                if ('external' === $request->get('type')) {
+                if ('external' === $request->attributes->get('type')) {
                     $path = '../' . $path;
                 }
 
@@ -62,7 +59,7 @@ namespace Neusta\Pimcore\DatabaseAdminBundle\Controller\Admin {
 
                 $filePath = $this->adminerHome . '/' . $path;
 
-                // it seems that css files need the right content-type (Chrome)
+                // it seems that CSS files need the right content-type (Chrome)
                 if (preg_match('@.css$@', $path)) {
                     $response->headers->set('Content-Type', 'text/css');
                 } elseif (preg_match('@.js$@', $path)) {
@@ -91,27 +88,19 @@ namespace Neusta\Pimcore\DatabaseAdminBundle\Controller\Admin {
                 return;
             }
 
-            // PHP 7.0 compatibility of adminer (throws some warnings)
-            ini_set('display_errors', 0);
-
             // only for admins
             $this->checkPermission('neusta_database_admin');
-
-            // call this to keep the session 'open' so that Adminer can write to it
-//            $session = Session::get();
 
             $this->adminerHome = PIMCORE_COMPOSER_PATH . '/vrana/adminer/';
         }
 
         /**
-         * Merges http-headers set from Adminer via headers function to the Symfony Response Object.
+         * Merges HTTP headers set from Adminer via headers function to the Symfony response object.
          */
         private function mergeAdminerHeaders(Response $response): Response
         {
             if (!headers_sent()) {
-                $headersRaw = headers_list();
-
-                foreach ($headersRaw as $header) {
+                foreach (headers_list() as $header) {
                     [$headerKey, $headerValue] = explode(':', $header, 2);
 
                     if ($headerKey && $headerValue) {
@@ -137,7 +126,7 @@ namespace {
             $pluginDir = PIMCORE_COMPOSER_PATH . '/vrana/adminer/plugins';
 
             // required to run any plugin
-            include_once $pluginDir . '/plugin.php';
+            require_once $pluginDir . '/plugin.php';
 
             // autoloader
             foreach (glob($pluginDir . '/*.php') as $filename) {
@@ -145,27 +134,27 @@ namespace {
             }
 
             $plugins = [
-                new \AdminerFrames(),
-                new \AdminerDumpDate,
-                new \AdminerDumpJson,
-                new \AdminerDumpBz2,
-                new \AdminerDumpZip,
-                new \AdminerDumpXml,
-                new \AdminerDumpAlter,
+                new AdminerFrames(),
+                new AdminerDumpDate(),
+                new AdminerDumpJson(),
+                new AdminerDumpBz2(),
+                new AdminerDumpZip(),
+                new AdminerDumpXml(),
+                new AdminerDumpAlter(),
             ];
 
             // support for SSL (at least for PDO)
-            $driverOptions = \Pimcore\Db::get()->getParams()['driverOptions'] ?? [];
+            $driverOptions = Pimcore\Db::get()->getParams()['driverOptions'] ?? [];
             $ssl = [
-                'key' => $driverOptions[\PDO::MYSQL_ATTR_SSL_KEY] ?? null,
-                'cert' => $driverOptions[\PDO::MYSQL_ATTR_SSL_CERT] ?? null,
-                'ca' => $driverOptions[\PDO::MYSQL_ATTR_SSL_CA] ?? null,
+                'key' => $driverOptions[PDO::MYSQL_ATTR_SSL_KEY] ?? null,
+                'cert' => $driverOptions[PDO::MYSQL_ATTR_SSL_CERT] ?? null,
+                'ca' => $driverOptions[PDO::MYSQL_ATTR_SSL_CA] ?? null,
             ];
             if (null !== $ssl['key'] || null !== $ssl['cert'] || null !== $ssl['ca']) {
-                $plugins[] = new \AdminerLoginSsl($ssl);
+                $plugins[] = new AdminerLoginSsl($ssl);
             }
 
-            class AdminerPimcore extends \AdminerPlugin
+            class AdminerPimcore extends AdminerPlugin
             {
                 public function name(): string
                 {
@@ -198,7 +187,7 @@ namespace {
 
                 public function credentials(): array
                 {
-                    $params = \Pimcore\Db::get()->getParams();
+                    $params = Pimcore\Db::get()->getParams();
 
                     $host = $params['host'] ?? null;
                     if ($port = $params['port'] ?? null) {
@@ -215,8 +204,8 @@ namespace {
 
                 public function database(): string
                 {
-                    // database name, will be escaped by Adminer
-                    return \Pimcore\Db::get()->getDatabase();
+                    // database name will be escaped by Adminer
+                    return Pimcore\Db::get()->getDatabase();
                 }
 
                 public function databases($flush = true): array
